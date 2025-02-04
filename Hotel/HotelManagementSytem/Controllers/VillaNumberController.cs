9d4f7d84ee99e1ef469dd114ef5d39704652557f
AfsarTanvir <afsartanvir805@gmail.com>
2025-02-04 20:18:48 +0600

    8th Commit ViewBeg, ViewData

---
+++ Hotel/HotelManagementSytem/Controllers/VillaNumberController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+
+    public class VillaNumberController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaNumberController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villas = _db.VillaNumbers.ToList();
+            return View(villas);
+        }
+        public IActionResult Create()
+        {
+            IEnumerable<SelectListItem> list = _db.Villas.ToList().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            ViewData["VillaList"] = list;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(VillaNumber obj)
+        {
+            // If I want to execute a model without a variable ModelState dot Remove("Villa");
+            if (ModelState.IsValid)
+            {
+                _db.VillaNumbers.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The Villa Number has been Created successfully.";
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "The Villa Number could not be Created.";
+            return View("Index", "VillaNumber");
+        }
+
+    }
+}
---
+++ Hotel/WhiteLagoon.Domain/Entities/VillaNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel;
+
+namespace WhiteLagoon.Domain.Entities
+{
+    public class VillaNumber
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Villa Number")]
+        public int Villa_Number { get; set; }
+
+        [ForeignKey("Villa")]
+        public int VillaId { get; set; }
+        [ValidateNever]
+        public Villa Villa { get; set; }
+        [DisplayName("Special Details")]
+        public string? SpecialDetails { get; set; }
+    }
+}
